2cbadf3dec1460f0ad03888b6d19c4df8e8cf9a1
Danin Sadžak <daninsadzak@gmail.com>
2022-10-24 19:42:25 +0200

    Added variables for My Baby's First Birthday

--- GenresAndStories/adventurestories.h
+++ GenresAndStories/adventurestories.h
@@ -3,5 +3,6 @@
 #include <string>
 using namespace std;
 void MyBabysFirstBirthday(){
-    
+    string nameofPerson1,nameofPerson2, place, adjective1, adjective2, adjective3, adjective4, adjective5, adjective6, adjective7, adjective8, adjective9, adjective10, adjective11, pluralNoun1, pluralNoun2, pluralNoun3, instrument, noun1, noun2, noun3, noun4, noun5, noun6, noun7, noun8, noun9, noun10, noun11, foodItem, mrOrMrs, upOrDown, animal1, animal2, animal3, pluralFictionalCharacter, pluralNameCalling, verbEndingInIng, clothingPlural, cartoonCharacter, verb1, verb2, verb3, transportation, roomInHouse, verbEndingInEd1, verbEndingInEd2, foodPlural, liquid, longOrShort, bodyPart;
+    float number1, number2;
 }
