e09a89152d1d9a610d934de938ba9cb92e3213d9
Danin Sadžak <daninsadzak@gmail.com>
2022-10-26 00:03:26 +0200

    Declared My Baby's First Birthday Variables

--- GenresAndStories/adventurestories.h
+++ GenresAndStories/adventurestories.h
@@ -5,4 +5,217 @@ using namespace std;
 void MyBabysFirstBirthday(){
     string nameofPerson1,nameofPerson2, place, adjective1, adjective2, adjective3, adjective4, adjective5, adjective6, adjective7, adjective8, adjective9, adjective10, adjective11, pluralNoun1, pluralNoun2, pluralNoun3, instrument, noun1, noun2, noun3, noun4, noun5, noun6, noun7, noun8, noun9, noun10, noun11, foodItem, mrOrMrs, upOrDown, animal1, animal2, animal3, pluralFictionalCharacter, pluralNameCalling, verbEndingInIng, clothingPlural, cartoonCharacter, verb1, verb2, verb3, transportation, roomInHouse, verbEndingInEd1, verbEndingInEd2, foodPlural, liquid, longOrShort, bodyPart;
     float number1, number2;
+    cout<<"Name of person: ";
+    cin.ignore();
+    getline(cin, nameofPerson1);
+
+    cout<<"Place: ";
+    cin.ignore(0, '\n');
+    getline(cin, place);
+
+    cout<<"Number: ";
+    cin>>number1;
+    cin.ignore(1, '\n');
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective10);
+
+    cout<<"Noun - Plural: ";
+    cin.ignore(0, '\n');
+    getline(cin, pluralNoun1);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective2);
+
+    cout<<"Instrument: ";
+    cin.ignore(0, '\n');
+    getline(cin, instrument);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun1);
+
+    cout<<"Food item: ";
+    cin.ignore(0, '\n');
+    getline(cin, foodItem);
+
+    cout<<"Mr or Mrs?: ";
+    cin.ignore(0, '\n');
+    getline(cin, mrOrMrs);
+
+    cout<<"Up or down?: ";
+    cin.ignore(0, '\n');
+    getline(cin, upOrDown);
+
+    cout<<"Noun - Plural: ";
+    cin.ignore(0, '\n');
+    getline(cin, pluralNoun2);
+
+    cout<<"Animal: ";
+    cin.ignore(0, '\n');
+    getline(cin, animal1);
+
+    cout<<"Plural fictional character: "
+    cin.ignore(0, '\n');
+    getline(cin, pluralFictionalCharacter);
+
+    cout<<"Plural name calling: ";
+    cin.ignore(0, '\n');
+    getline(cin, pluralNameCalling);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective3);
+
+    cout<<"Verb ending in ing: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbEndingInIng);
+
+    cout<<"Clothing (plural): ";
+    cin.ignore(0, '\n');
+    getline(cin, clothingPlural);
+
+    cout<<"Cartoon character: ";
+    cin.ignore(0, '\n');
+    getline(cin, cartoonCharacter);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun2);
+
+    cout<<"Adjective: "; 
+    cin.ignore(0, '\n');
+    getline(cin, adjective4);
+
+    cout<<"Adjective: "; 
+    cin.ignore(0, '\n');
+    getline(cin, adjective5);
+
+    cout<<"Verb: ";
+    cin.ignore(0, '\n');
+    getline(cin, verb1);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun3);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun4);
+
+    cout<<"Verb: ";
+    cin.ignore(0, '\n');
+    getline(cin, verb2);
+
+    cout<<"Verb: ";
+    cin.ignore(0, '\n');
+    getline(cin, verb3);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun5);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun6);
+
+    cout<<"Noun - Plural: ";
+    cin.ignore(0, '\n');
+    getline(cin, pluralNoun3);
+
+    cout<<"Transportation: ";
+    cin.ignore(0, '\n');
+    getline(cin, transportation);
+
+    cout<<"Room in house: ";
+    cin.ignore(0, '\n');
+    getline(cin, roomInHouse);
+
+    cout<<"Verb ending in ed: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbEndingInEd1);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective6);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective7);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective8);
+
+    cout<<"Number: ";
+    cin>>number2;
+    cin.ignore(1, '\n');
+
+    cout<<"Name of a person: ";
+    cin.ignore(0, '\n');
+    getline(cin, nameofPerson2);
+
+    cout<<"Animal: ";
+    cin.ignore(0, '\n');
+    getline(cin, animal2);
+
+    cout<<"Food (plural): ";
+    cin.ignore(0, '\n');
+    getline(cin, foodPlural);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun7);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun8);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun9);
+    
+    cout<<"Liquid: ";
+    cin.ignore(0, '\n');
+    getline(cin, liquid);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective9);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun10);
+
+    cout<<"Long or short?: ";
+    cin.ignore(0, '\n');
+    getline(cin, longOrShort);
+
+    cout<<"Animal: ";
+    cin.ignore(0, '\n');
+    getline(cin, animal3);
+
+    cout<<"Verb ending with ed: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbEndingInEd2);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun11);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective10);
+
+    cout<<"Body part: ";
+    cin.ignore(0, '\n');
+    getline(cin, bodyPart);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective11);
+
+    
 }
