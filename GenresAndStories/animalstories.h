02926c6b5acc1140d5771caa9147155b8a34adb5
Danin Sadžak <daninsadzak@gmail.com>
2022-11-05 10:50:56 +0100

    Added variables for Dog Off Leash

--- GenresAndStories/animalstories.h
+++ GenresAndStories/animalstories.h
@@ -4,5 +4,7 @@
 using namespace std;
 //WIP
 void DogOffLeash(){
-    
+    system("CLS");
+    string pluralNoun1, pluralNoun2, pluralNoun3, pluralNoun4, adjective1, adjective2, adjective3, adjective4, adjective5, adjective6, adjective7, adjective8, verbBaseForm1, verbBaseForm2, verbBaseForm3, verbBaseForm4, verbBaseForm5, verbBaseForm6, shoutOrYell1, shoutOrYell2, food1, food2, food3, food4, noun1, noun2, bodyPart, verbPresentEndsInIng1, VerbPresentEndsInIng2, verbPresentEndsInIng3, verbPresentEndsInS, animal1, animal2, plantOrFlower;
+     
 }
