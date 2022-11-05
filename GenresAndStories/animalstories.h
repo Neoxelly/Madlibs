b91cdb1088f7668cfb507c732185c6adf03a9e16
Danin Sadžak <daninsadzak@gmail.com>
2022-11-05 11:02:26 +0100

    Initialized variables for Dog Off Leash

--- GenresAndStories/animalstories.h
+++ GenresAndStories/animalstories.h
@@ -6,5 +6,140 @@ using namespace std;
 void DogOffLeash(){
     system("CLS");
     string pluralNoun1, pluralNoun2, pluralNoun3, pluralNoun4, adjective1, adjective2, adjective3, adjective4, adjective5, adjective6, adjective7, adjective8, verbBaseForm1, verbBaseForm2, verbBaseForm3, verbBaseForm4, verbBaseForm5, verbBaseForm6, shoutOrYell1, shoutOrYell2, food1, food2, food3, food4, noun1, noun2, bodyPart, verbPresentEndsInIng1, VerbPresentEndsInIng2, verbPresentEndsInIng3, verbPresentEndsInS, animal1, animal2, plantOrFlower;
-     
+
+    cout<<"Noun - Plural: ";
+    cin.ignore();
+    getline(cin, pluralNoun1);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective1);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective2);
+
+    cout<<"Verb - base form: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbBaseForm1);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective3);
+
+    cout<<"Verb - base form: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbBaseForm2);
+
+    cout<<"Something you shout or yell: ";
+    cin.ignore(0, '\n');
+    getline(cin, shoutOrYell1); 
+
+    cout<<"Something you shout or yell: ";
+    cin.ignore(0, '\n');
+    getline(cin, shoutOrYell2);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective4);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective5);
+
+    cout<<"Food: ";
+    cin.ignore(0, '\n');
+    getline(cin, food1);
+
+    cout<<"Food: ";
+    cin.ignore(0, '\n');
+    getline(cin, food2);
+
+    cout<<"Food: ";
+    cin.ignore(0, '\n');
+    getline(cin, food3);
+
+    cout<<"Food: ";
+    cin.ignore(0, '\n');
+    getline(cin, food4);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective6);
+
+    cout<<"Noun - Plural: ";
+    cin.ignore(0, '\n');
+    getline(cin, pluralNoun2);
+
+    cout<<"Verb - base form: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbBaseForm3);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun1);
+
+    cout<<"Body part: ";
+    cin.ignore(0, '\n');
+    getline(cin, bodyPart);
+
+    cout<<"Verb - base form: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbBaseForm4);
+
+    cout<<"Verb - base form: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbBaseForm5);
+
+    cout<<"Verb - Present ends in ING: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbPresentEndsInIng1);
+
+    cout<<"Verb - Present ends in S: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbPresentEndsInS);
+
+    cout<<"Animal: ";
+    cin.ignore(0, '\n');
+    getline(cin, animal1);
+
+    cout<<"Plant or flower: ";
+    cin.ignore(0, '\n');
+    getline(cin, plantOrFlower);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective7);
+
+    cout<<"Animal: ";
+    cin.ignore(0, '\n');
+    getline(cin, animal2);
+
+    cout<<"Verb - present ends in ING: ";
+    cin.ignore(0, '\n');
+    getline(cin, VerbPresentEndsInIng2);
+
+    cout<<"Verb - base form: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbBaseForm6);
+
+    cout<<"Noun: ";
+    cin.ignore(0, '\n');
+    getline(cin, noun2);
+
+    cout<<"Adjective: ";
+    cin.ignore(0, '\n');
+    getline(cin, adjective8);
+
+    cout<<"Noun - Plural: ";
+    cin.ignore(0, '\n');
+    getline(cin, pluralNoun3);
+
+    cout<<"Verb - present ends in ING: ";
+    cin.ignore(0, '\n');
+    getline(cin, verbPresentEndsInIng3);
+
+    cout<<"Noun - Plural: ";
+    cin.ignore(0, '\n');
+    getline(cin, pluralNoun4);  
 }
